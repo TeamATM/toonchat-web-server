2cee479e39261d19bcdc8da2ef277283943a04e9
MustSave <dbstjdgk1541@gmail.com>
2023-07-31 15:12:46 +0900

    Chore: Spring Boot 초기 설정

---
+++ src/main/java/com/webtoonchat/toonchat/ToonchatApplication.java
@@ -0,0 +1,13 @@
+package com.webtoonchat.toonchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToonchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webtoonchat/toonchat/ToonchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtoonchat.toonchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToonchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
