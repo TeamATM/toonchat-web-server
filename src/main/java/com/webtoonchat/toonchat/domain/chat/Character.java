fd4cb0b5c89fbdde216461378a5621a35d0f431e
jaewonkim <resource777@naver.com>
2023-08-03 00:13:51 +0900

    Feat: Character domain 생성

--- src/main/java/com/webtoonchat/toonchat/domain/chat/Character.java
+++ src/main/java/com/webtoonchat/toonchat/domain/chat/Character.java
@@ -1,4 +1,21 @@
 package com.webtoonchat.toonchat.domain.chat;
 
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "character")
 public class Character {
+	@Id
+	private String id;
+	private String botName;
+	private String hashtag;
+	private String imageUrl;
 }
