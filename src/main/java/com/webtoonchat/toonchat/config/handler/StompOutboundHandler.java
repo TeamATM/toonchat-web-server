68fb7d1121dd2499f4dee49375d2993618734386
jaewonkim <resource777@naver.com>
2023-08-05 12:27:58 +0900

    Feat: handler에서 Dto 값 세팅 후 디비 저장

--- src/main/java/com/webtoonchat/toonchat/config/handler/StompOutboundHandler.java
+++ src/main/java/com/webtoonchat/toonchat/config/handler/StompOutboundHandler.java
@@ -12,26 +12,43 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.webtoonchat.toonchat.SessionUtils;
 import com.webtoonchat.toonchat.dto.StompMessageDto;
+import com.webtoonchat.toonchat.service.chat.StompMessageService;
 
 import lombok.RequiredArgsConstructor;
 
-@Component
 @RequiredArgsConstructor
+@Component
 public class StompOutboundHandler implements ChannelInterceptor {
-	private StringMessageConverter converter = new StringMessageConverter();
+	private final ObjectMapper objectMapper = new ObjectMapper();
+	private final StompMessageService stompMessageService;
 	@Override
 	public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+		String username = SessionUtils.getUserName();
+		System.out.println("handler에서 찾습니다. username = " + username);
 
 		StompCommand command = accessor.getCommand();
-		System.out.println("command = " + command);
-		System.out.println("message = " + new String((byte[]) message.getPayload()));
-		System.out.println("command = " + command);
 		if (StompCommand.MESSAGE.equals(command)) {
-			// Handle CONNECT command
-			System.out.println("123456789");
+			String jsonPayload = new String((byte[]) message.getPayload());
+			StompMessageDto stompMessageDto = convertMessageToStompDto(jsonPayload);
+			stompMessageDto.setReplyMessageId()
+				.setMessageTo(username)
+				.setStatus("Outbound Handler Create This");
+			// Insert into database
+			System.out.println("stompMessageDto.getContent() = " + stompMessageDto.getContent());
+			stompMessageService.save(stompMessageDto);
 		}
 	}
 
+	public StompMessageDto convertMessageToStompDto(String payload) {
+		try {
+			return objectMapper.readValue(payload, StompMessageDto.class);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException("Error deserializing JSON payload", e);
+		}
+	}
 }
