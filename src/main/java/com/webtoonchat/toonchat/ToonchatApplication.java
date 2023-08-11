2af79ad2a252379e751670119dc8cc9ff5ea06a8
jaewonkim <resource777@naver.com>
2023-08-11 14:12:05 +0900

    Fix: swagger https 문제 해결

--- src/main/java/com/webtoonchat/toonchat/ToonchatApplication.java
+++ src/main/java/com/webtoonchat/toonchat/ToonchatApplication.java
@@ -1,11 +1,14 @@
 package com.webtoonchat.toonchat;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://dev.webtoonchat.com")})
 @EnableJpaAuditing
 @EnableMongoAuditing
 @EnableMongoRepositories
