6c01af693d516b14ccd26cff12f019ce42a41b43
jaewonkim <resource777@naver.com>
2023-08-11 13:55:50 +0900

    Chore: to check api of anonymous  client

--- src/main/java/com/webtoonchat/toonchat/controller/chat/ChatApiController.java
+++ src/main/java/com/webtoonchat/toonchat/controller/chat/ChatApiController.java
@@ -47,7 +47,7 @@ public class ChatApiController {
 	@GetMapping("/api/chat/{id}/history")
 	public ResponseEntity<List<StompMessageEntity>> getChatHistory(@PathVariable String id) {
 		String username = SessionUtils.getUserName();
-		username = "anonymous";
+		username = "anonymous-8b015168-f432-477a-a830-0c6229ef1af6";
 
 		Optional<Character> character = characterService.getCharacterInfo(id);
 		String characterName = character.map(Character::getBotName).orElse("There is No bot to talk");
