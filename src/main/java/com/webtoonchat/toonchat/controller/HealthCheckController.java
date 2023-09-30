7006d173d7a72bfe0bbeec7044be6286fc7216af
resource777 <79133019+resource777@users.noreply.github.com>
2023-09-30 14:57:27 +0900

    Feat: health check controller 추가

---
+++ src/main/java/com/webtoonchat/toonchat/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.webtoonchat.toonchat.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+	private final ResponseEntity<String> healthResponseEntity = ResponseEntity.ok().body("OK");
+
+	@GetMapping
+	public ResponseEntity<String> health() {
+		return healthResponseEntity;
+	}
+}
