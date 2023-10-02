1f8428f5dfaaaf984477027f3ff04decbffc8266
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-02 17:10:21 +0900

    Fix: cors 허용범위 수정

--- src/main/java/com/webtoonchat/toonchat/config/WebConfig.java
+++ src/main/java/com/webtoonchat/toonchat/config/WebConfig.java
@@ -25,7 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:8080", "http://webtoonchat.com")
+				.allowedOrigins("http://localhost:8080", "https://webtoonchat.com",
+						"http://localhost:3000", "http://dev.webtoonchat.com")
 				.allowedMethods("GET", "POST", "PATCH", "PUT", "OPTIONS", "DELETE")
 				.allowCredentials(true);
 	}
