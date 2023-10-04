d95355b7ecfb2d01c37e51d53c1eb9cd0a7d2e7c
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-04 14:10:23 +0900

    Fix: security config cors disable

--- src/main/java/com/webtoonchat/toonchat/config/SecurityConfig.java
+++ src/main/java/com/webtoonchat/toonchat/config/SecurityConfig.java
@@ -34,8 +34,7 @@ public class SecurityConfig {
 				.and()
 				.formLogin().disable() // 직접 id, password를 입력받아서 JWT토큰을 리턴하는 API를 직접 만든다.
 				.csrf().disable() // CSRF는 Cross Site Request Forgery의 약자. CSRF공격을 막기 위한 방법.
-				.cors()//.configurationSource(corsConfigurationSource())
-				.and()
+				.cors().disable()//.configurationSource(corsConfigurationSource())
 				.apply(authenticationManagerConfig)
 				.and()
 				.httpBasic().disable()
