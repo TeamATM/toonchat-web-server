347869d24b858cf922901efb2393688cb4821161
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-05 19:28:30 +0900

    Chore: mongo dependency removed

--- src/main/java/com/webtoonchat/toonchat/ToonchatApplication.java
+++ src/main/java/com/webtoonchat/toonchat/ToonchatApplication.java
@@ -3,15 +3,11 @@ package com.webtoonchat.toonchat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 @OpenAPIDefinition(servers = {@Server(url = "https://dev.webtoonchat.com")})
 @EnableJpaAuditing
-@EnableMongoAuditing
-@EnableMongoRepositories
 @SpringBootApplication
 public class ToonchatApplication {
 
