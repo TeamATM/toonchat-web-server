ce8064081e5ef998344a2d836d59c87ec81507cc
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-10 23:29:23 +0900

    Fix: bgno -> characterId

--- src/main/java/com/webtoonchat/toonchat/controller/board/BoardApiController.java
+++ src/main/java/com/webtoonchat/toonchat/controller/board/BoardApiController.java
@@ -37,20 +37,21 @@ public class BoardApiController {
 	@Value("${jwt.secretKey}")
 	private String secretKey;
 
-	@PostMapping("/api/boards/{bgno}")
+	@PostMapping("/api/boards/{characterId}")
 	public ResponseEntity<Board> addBoard(
-			@PathVariable String bgno, @RequestBody AddBoardRequest request, HttpServletRequest httpServletRequest) {
+			@PathVariable String characterId,
+			@RequestBody AddBoardRequest request, HttpServletRequest httpServletRequest) {
 		Claims claims = extracted(httpServletRequest);
 		Integer userid = (Integer) claims.get("userId");
-		Board savedArticle = boardService.save(request, bgno, userid);
+		Board savedArticle = boardService.save(request, characterId, userid);
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(savedArticle);
 	}
 
-	@GetMapping("/api/boards/{bgno}")
+	@GetMapping("/api/boards/{characterId}")
 	public ResponseEntity<List<BoardResponse>> findAllBoards(
-			@PathVariable String bgno, HttpServletRequest httpServletRequest) {
-		List<BoardResponse> articles = boardService.findAllByBgno(bgno)
+			@PathVariable String characterId, HttpServletRequest httpServletRequest) {
+		List<BoardResponse> articles = boardService.findAllByBgno(characterId)
 				.stream()
 				.map(BoardResponse::new)
 				.toList();
@@ -59,18 +60,18 @@ public class BoardApiController {
 				.body(articles);
 	}
 
-	@GetMapping("/api/boards/{bgno}/{id}")
+	@GetMapping("/api/boards/{characterId}/{id}")
 	public ResponseEntity<BoardResponse> findBoard(
-			@PathVariable String bgno, @PathVariable long id, HttpServletRequest httpServletRequest) {
+			@PathVariable String characterId, @PathVariable long id, HttpServletRequest httpServletRequest) {
 		Board article = boardService.findById(id);
 
 		return ResponseEntity.ok()
 				.body(new BoardResponse(article));
 	}
 
-	@DeleteMapping("/api/boards/{bgno}/{id}")
+	@DeleteMapping("/api/boards/{characterId}/{id}")
 	public ResponseEntity<Void> deleteBoard(
-			@PathVariable String bgno, @PathVariable long id, HttpServletRequest httpServletRequest) {
+			@PathVariable String characterId, @PathVariable long id, HttpServletRequest httpServletRequest) {
 		Claims claims = extracted(httpServletRequest);
 		if (boardService.findById(id).getWriterId() != (int)claims.get("userId")) {
 			log.error("작성자가 아닌 글 삭제 요청");
@@ -82,9 +83,9 @@ public class BoardApiController {
 				.build();
 	}
 
-	@PutMapping("/api/boards/{bgno}/{id}")
+	@PutMapping("/api/boards/{characterId}/{id}")
 	public ResponseEntity<Board> updateBoard(
-			@PathVariable String bgno,
+			@PathVariable String characterId,
 			@PathVariable long id,
 			@RequestBody UpdateBoardRequest request,
 			HttpServletRequest httpServletRequest
