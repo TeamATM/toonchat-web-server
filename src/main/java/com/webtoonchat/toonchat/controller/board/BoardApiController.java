86de2f15bdea85699555a6f10a2a2128b55b875f
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-12 01:19:47 +0900

    Feat: swagger description 추가

--- src/main/java/com/webtoonchat/toonchat/controller/board/BoardApiController.java
+++ src/main/java/com/webtoonchat/toonchat/controller/board/BoardApiController.java
@@ -1,13 +1,11 @@
 package com.webtoonchat.toonchat.controller.board;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
