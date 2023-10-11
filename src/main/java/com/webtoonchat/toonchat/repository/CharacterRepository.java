4181f68fe449e3669b542e5c95210e0b39e32dbc
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-12 00:17:18 +0900

    Fix: Character hashtags String added

--- src/main/java/com/webtoonchat/toonchat/repository/CharacterRepository.java
+++ src/main/java/com/webtoonchat/toonchat/repository/CharacterRepository.java
@@ -1,8 +1,17 @@
 package com.webtoonchat.toonchat.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.webtoonchat.toonchat.domain.Character;
 
+
 public interface CharacterRepository extends JpaRepository<Character, Long> {
+	public List<Character> findAll();
+
+	public Optional<Character> findByCharacterId(String characterId);
+
+
 }
