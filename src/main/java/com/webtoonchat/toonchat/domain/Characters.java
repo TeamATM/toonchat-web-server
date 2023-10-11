e4b03937fa875b8f8fd5b9b8f495f35cf3a82733
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-12 01:19:47 +0900

    Feat: swagger description 추가

--- src/main/java/com/webtoonchat/toonchat/domain/Characters.java
+++ src/main/java/com/webtoonchat/toonchat/domain/Characters.java
@@ -8,12 +8,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Table(name = "characters")
 @NoArgsConstructor
 @Entity
-public class Character {
+public class Characters {
 	@Id
 	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
