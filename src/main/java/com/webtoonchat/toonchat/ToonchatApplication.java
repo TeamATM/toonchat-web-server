5810fca9c4ea26cdeac09ad3db170d27f89c24be
MustSave <dbstjdgk1541@gmail.com>
2023-10-15 17:00:45 +0900

    Feat: update swagger docs to use Authorization header

--- src/main/java/com/webtoonchat/toonchat/ToonchatApplication.java
+++ src/main/java/com/webtoonchat/toonchat/ToonchatApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
-@OpenAPIDefinition(servers = {@Server(url = "https://dev.webtoonchat.com")})
+@OpenAPIDefinition(servers = {@Server(url = "https://dev.webtoonchat.com"), @Server(url = "http://localhost:8080")})
 @EnableJpaAuditing
 @SpringBootApplication
 public class ToonchatApplication {
--- src/main/java/com/webtoonchat/toonchat/config/swagger/SwaggerConfig.java
+++ src/main/java/com/webtoonchat/toonchat/config/swagger/SwaggerConfig.java
@@ -3,17 +3,26 @@ package com.webtoonchat.toonchat.config.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 
+@SecurityScheme(
+	type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT",
+	in = SecuritySchemeIn.HEADER, name = "Authorization"
+)
 @Configuration
 public class SwaggerConfig {
 	@Bean
 	public OpenAPI openApi() {
 		return new OpenAPI()
-				.components(new Components())
-				.info(apiInfo());
+			.components(new Components())
+			.addSecurityItem(new SecurityRequirement().addList("Authorization"))
+			.info(apiInfo());
 	}
 
 	private Info apiInfo() {
