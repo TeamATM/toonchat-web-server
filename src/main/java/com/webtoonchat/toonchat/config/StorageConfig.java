77035cb19e6cbdb0364fd82353ca319b2aa68beb
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-16 12:27:14 +0900

    Chore: s3 일단 주석 처리

--- src/main/java/com/webtoonchat/toonchat/config/StorageConfig.java
+++ src/main/java/com/webtoonchat/toonchat/config/StorageConfig.java
@@ -13,19 +13,19 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 @Configuration
 public class StorageConfig {
 
-	@Value("${cloud.aws.credentials.access-key}")
-	private String accessKey;
-
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String accessSecret;
-	@Value("${cloud.aws.region.static}")
-	private String region;
-
-	@Bean
-	public AmazonS3 s3Client() {
-		AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
-		return AmazonS3ClientBuilder.standard()
-				.withCredentials(new AWSStaticCredentialsProvider(credentials))
-				.withRegion(region).build();
-	}
+//	@Value("${cloud.aws.credentials.access-key}")
+//	private String accessKey;
+//
+//	@Value("${cloud.aws.credentials.secret-key}")
+//	private String accessSecret;
+//	@Value("${cloud.aws.region.static}")
+//	private String region;
+//
+//	@Bean
+//	public AmazonS3 s3Client() {
+//		AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
+//		return AmazonS3ClientBuilder.standard()
+//				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+//				.withRegion(region).build();
+//	}
 }
