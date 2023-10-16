0e3c4ca568fdf3fd6260d2226ebc0cb42239f71f
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-16 12:38:16 +0900

    Feat: Friendship entity 생성

---
+++ src/main/java/com/webtoonchat/toonchat/domain/Friendship.java
@@ -0,0 +1,32 @@
+package com.webtoonchat.toonchat.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "friendship")
+@NoArgsConstructor
+public class Friendship {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "character_id")
+	private Characters character;
+}
