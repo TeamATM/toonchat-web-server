ae4bdbf59d10480a24d12128e45004e09445a661
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-16 16:34:55 +0900

    Feat: member character n:m 매핑

--- src/main/java/com/webtoonchat/toonchat/repository/FriendshipRepository.java
+++ src/main/java/com/webtoonchat/toonchat/repository/FriendshipRepository.java
@@ -1,11 +1,16 @@
 package com.webtoonchat.toonchat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.webtoonchat.toonchat.domain.Friendship;
 
+
 @Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+	List<Friendship> findByMemberMemberId(Long memberId);
+
 }
 
