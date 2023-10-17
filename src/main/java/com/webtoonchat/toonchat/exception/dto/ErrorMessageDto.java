bdd52010e42711a34ccf38989be5af999776c517
MustSave <dbstjdgk1541@gmail.com>
2023-10-17 17:59:10 +0900

    Feat: add global exception handler for NoSuchElementException

---
+++ src/main/java/com/webtoonchat/toonchat/exception/dto/ErrorMessageDto.java
@@ -0,0 +1,11 @@
+package com.webtoonchat.toonchat.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDto {
+	private String error;
+}
