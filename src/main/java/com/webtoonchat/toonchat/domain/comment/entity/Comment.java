785fcc012523c86f8d0dbd36d41aeb8a9785f88b
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-22 16:53:34 +0900

    Feat: comment dto added and board entity modified

--- src/main/java/com/webtoonchat/toonchat/domain/comment/entity/Comment.java
+++ src/main/java/com/webtoonchat/toonchat/domain/comment/entity/Comment.java
@@ -15,6 +15,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,6 +25,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "\"comment\"")
 @Entity
+@Builder
 public class Comment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
