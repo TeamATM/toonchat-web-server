8b8ee3b00a6cc8ed6e8bc6bcea80d1b38a12f06f
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-22 16:30:49 +0900

    Feat: comment entity added

---
+++ src/main/java/com/webtoonchat/toonchat/domain/comment/entity/Comment.java
@@ -0,0 +1,50 @@
+package com.webtoonchat.toonchat.domain.comment.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.webtoonchat.toonchat.domain.board.entity.Board;
+import com.webtoonchat.toonchat.domain.member.entity.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "\"comment\"")
+@Entity
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String comment; // 댓글 내용
+
+	@Column(name = "created_date")
+	@CreatedDate
+	private String createdDate;
+
+	@Column(name = "modified_date")
+	@LastModifiedDate
+	private String modifiedDate;
+
+	@ManyToOne
+	@JoinColumn(name = "board_id")
+	private Board board;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
+}
