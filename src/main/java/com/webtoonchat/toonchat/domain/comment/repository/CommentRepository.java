8571c8f080daf3d1b1dfd0ea9fdd79eff512bbc1
resource777 <79133019+resource777@users.noreply.github.com>
2023-10-25 17:47:29 +0900

    Feat: 댓글 RUD 구현

--- src/main/java/com/webtoonchat/toonchat/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/webtoonchat/toonchat/domain/comment/repository/CommentRepository.java
@@ -1,10 +1,15 @@
 package com.webtoonchat.toonchat.domain.comment.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.webtoonchat.toonchat.domain.comment.entity.Comment;
 
 
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+	List<Comment> findAllByBoardId(Long boardId);
+
 }
 
