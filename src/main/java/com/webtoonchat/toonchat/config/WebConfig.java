d576b7911289ddbc444bc1a61d3bb241a0f968fc
MustSave <dbstjdgk1541@gmail.com>
2023-11-01 15:43:17 +0900

    Chore: Remove unnecessary code

--- src/main/java/com/webtoonchat/toonchat/config/WebConfig.java
+++ src/main/java/com/webtoonchat/toonchat/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.webtoonchat.toonchat.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,20 +14,6 @@ import com.webtoonchat.toonchat.resolver.LoginArgumentResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/swagger-ui/**")
-				.addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.52.5/")
-				.resourceChain(false);
-	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/swagger-ui/")
-				.setViewName("forward:/swagger-ui/index.html");
-	}
-
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
