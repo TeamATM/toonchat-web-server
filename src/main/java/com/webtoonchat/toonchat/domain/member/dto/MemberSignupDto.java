6f11c40e3d5b95c27be75fe6c845a0b86da5d231
MustSave <dbstjdgk1541@gmail.com>
2023-11-01 17:20:31 +0900

    Chore: remove pattern of member name, fix regex of member email

--- src/main/java/com/webtoonchat/toonchat/domain/member/dto/MemberSignupDto.java
+++ src/main/java/com/webtoonchat/toonchat/domain/member/dto/MemberSignupDto.java
@@ -2,6 +2,7 @@ package com.webtoonchat.toonchat.domain.member.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,15 +15,14 @@ import lombok.NoArgsConstructor;
 public class MemberSignupDto {
 
 	@NotEmpty
-	@Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+	@Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
 			message = "이메일 형식을 맞춰야합니다")
 	private String email;
 
 	private String password;
 
 	@NotEmpty
-	@Pattern(regexp = "^[a-zA-Z가-힣\\\\s]{2,15}",
-			message = "이름은 영문자, 한글, 공백포함 2글자부터 15글자까지 가능합니다.")
+	@Size(min = 2, max = 15, message = "이름은 2글자부터 15글자까지 가능합니다.")
 	private String name;
 
 	private String provider;
