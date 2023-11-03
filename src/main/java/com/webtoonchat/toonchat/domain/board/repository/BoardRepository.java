d8b727ab21c0dc11b34ca26d4bbb6d1c16c42a5d
resource777 <79133019+resource777@users.noreply.github.com>
2023-11-03 19:57:24 +0900

    Fix: n+1 problem solved and boardview listing order reversed

--- src/main/java/com/webtoonchat/toonchat/domain/board/repository/BoardRepository.java
+++ src/main/java/com/webtoonchat/toonchat/domain/board/repository/BoardRepository.java
@@ -3,11 +3,14 @@ package com.webtoonchat.toonchat.domain.board.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.webtoonchat.toonchat.domain.board.entity.Board;
 
 
-
 public interface BoardRepository extends JpaRepository<Board, Long> {
-	List<Board> findAllByCharacterId(Long characterId);
+	@Query("select b "
+			+ "from Board b left join fetch b.comments c left join fetch c.member "
+			+ "where b.characterId=:characterId order by b.id desc")
+	List<Board> findAllByCharacterIdOrderByIdDesc(Long characterId);
 }
